26b7b8ea09381649a8fccf786d1bf16324e63039
faizakailani <74226971+faizakailani@users.noreply.github.com>
2021-01-14 11:10:33 +0700

    Mempelajari Widget di Flutter

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(Main());
+}
+
+class Main extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      theme: ThemeData.light(),
+      home: MyApp(),
+    );
+    throw UnimplementedError();
+  }
+
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Belajar Widget"),
+      ),
+      body:Container(
+        child:  Form(
+          child: Column(
+            children: <Widget>[
+              TextFormField(
+                decoration: InputDecoration(
+                    hintText: "Username"
+                ),
+              ),
+              TextFormField(
+                decoration: InputDecoration(
+                    hintText: "Password"
+                ),
+                obscureText: true,
+              ),
+              RaisedButton
+                (onPressed: null,
+                child: Text("Login",
+                style: TextStyle(
+                  color: Colors.white
+                ),),
+              ),
+            ],
+          ),
+        ),
+      )
+    );
+  }
+}
+
